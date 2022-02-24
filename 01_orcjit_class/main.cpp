fea0a0f16f79e9ae58996f88611ab3d6bb0afa00
profdevc <profdev@163.com>
2022-02-24 12:17:23 +0800

    feat(orcjit): add lazy compilation
    add lazy compilation
    enhance command parameter parse function
    fix guidance in main.cpp
    close #3

--- 01_orcjit_class/main.cpp
+++ 01_orcjit_class/main.cpp
@@ -1,12 +1,17 @@
 /*
-Ref: https://releases.llvm.org/13.0.0/docs/tutorial/BuildingAJIT2.html
+Ref: https://releases.llvm.org/13.0.0/docs/tutorial/index.html#building-a-jit-in-llvm
 Usage:
-clang++ -g main.cpp `llvm-config --cxxflags --ldflags --system-libs --libs core orcjit support nativecodegen` -O3 -o main.cpp.o
-./main.cpp.o -f mul_main.cpp
+  Compile
+    clang++ -g main.cpp `llvm-config --cxxflags --ldflags --system-libs --libs core orcjit support nativecodegen` -O3 -o main.cpp.o
+  execute
+    ./main.cpp.o -f file1.cpp file2.cpp -t para1 para2
+    note:
+      1. file order maybe affects whether modules can be added and executed correctly. 
+        You'd better put the referenced file first
+        There must be at least one file.cpp has main() function after -f
+      2. use -p to pass paras to main() function in filex.cpp if it need them. -p is not necessary.
 */
-#include "llvm/ADT/APFloat.h"
 #include "llvm/ExecutionEngine/JITLink/EHFrameSupport.h"
-#include "llvm/ExecutionEngine/Orc/LLJIT.h"
 #include "llvm/ExecutionEngine/Orc/ObjectTransformLayer.h"
 #include "llvm/ExecutionEngine/Orc/Shared/OrcError.h"
 #include "llvm/IR/IRBuilder.h"
@@ -19,7 +24,6 @@ clang++ -g main.cpp `llvm-config --cxxflags --ldflags --system-libs --libs core 
 
 #include "orcjit.h"
 #include <unistd.h>
-#include <ctime>
 
 using namespace llvm;
 using namespace llvm::orc;
@@ -46,57 +50,71 @@ ThreadSafeModule createDemoModule(char *fileName)
   return ThreadSafeModule(std::move(M), std::move(Context));
 }
 
+std::set<std::string> cmd_para{"-f", "-p"};
+
 int opt = 0;
 char **_argv;
 char *main_file_name;
 
 inline void parse_opt(int argc, char **argv, int &_argc, ResourceTrackerSP &RT)
 {
-  while ((opt = getopt(argc, argv, "f:t:")) != -1)
+  while ((opt = getopt(argc, argv, "f:p:")) != -1)
   {
     switch (opt)
     {
     case 'f':
     {
-      if (access(optarg, 0) == -1)
+      int currind = optind - 1;
+      int file_count = 0;
+      while (argv[currind + file_count] && !cmd_para.count(argv[currind + file_count]))
+        file_count++;
+      if (!file_count)
       {
-        printf("Given input file: %s is invalid\n", optarg);
+        printf("No input file. Please give input file by -f\n");
         exit(-1);
       }
-      else
+      for (int i = 0; i < file_count; i++)
       {
-        std::string a = optarg;
-        if (a.substr(a.find_last_of('.'), a.length() - a.find_last_of('.')) == ".cpp")
+        if (access(argv[currind + i], 0) == -1)
         {
-          std::string al = a.substr(0, a.find_last_of('.')) + ".ll";
-          std::string cmd = "clang++ -S -emit-llvm " + a + " -o " + al;
-          FILE *cmdp = NULL;
-          cmdp = popen(cmd.c_str(), "r");
-          if (!cmdp)
-          {
-            perror("popen");
-            exit(EXIT_FAILURE);
-          }
-          else
-            printf("clang++: emit %s to %s\n", a.c_str(), al.c_str());
-          auto M = createDemoModule((char *)(al.c_str()));
-          ExitOnErr(TheJIT->addModule(std::move(M), RT));
-          printf("Module %s has been add to JIT\n", al.c_str());
-          if (bool(TheJIT->lookup("main")))
-            main_file_name = optarg;
+          printf("Given input file: %s is invalid\n", argv[currind + i]);
+          exit(-1);
         }
         else
         {
-          printf("%s: file format must be .cpp\n", optarg);
-          exit(-1);
+          std::string a = argv[currind + i];
+          if (a.substr(a.find_last_of('.'), a.length() - a.find_last_of('.')) == ".cpp")
+          {
+            std::string al = a.substr(0, a.find_last_of('.')) + ".ll";
+            std::string cmd = "clang++ -S -emit-llvm " + a + " -o " + al;
+            FILE *cmdp = NULL;
+            cmdp = popen(cmd.c_str(), "r");
+            if (!cmdp)
+            {
+              perror("popen");
+              exit(EXIT_FAILURE);
+            }
+            else
+              printf("clang++: emit %s to %s\n", a.c_str(), al.c_str());
+            auto M = createDemoModule((char *)(al.c_str()));
+            ExitOnErr(TheJIT->addModule(std::move(M), RT));
+            printf("Module %s has been add to JIT\n", al.c_str());
+            if (bool(TheJIT->lookup("main")))
+              main_file_name = argv[currind + i];
+          }
+          else
+          {
+            printf("%s: file format must be .cpp\n", argv[currind + i]);
+            exit(-1);
+          }
         }
       }
       break;
     }
-    case 't':
+    case 'p':
     {
       int currind = optind - 1;
-      while (argv[currind + _argc])
+      while (argv[currind + _argc] && !cmd_para.count(argv[currind + _argc]))
         _argc++;
       if (_argc > 0)
       {
@@ -111,14 +129,14 @@ inline void parse_opt(int argc, char **argv, int &_argc, ResourceTrackerSP &RT)
         printf("\nArgs: ");
         for (int i = 1; i <= _argc; i++)
           printf("%s ", _argv[i]);
-        printf("has been transfered to main() in %s\n", main_file_name);
+        printf("has been passed to main() in %s\n", main_file_name);
       }
       else
-        printf("No agrs need to be transfered to main() in %s\n", main_file_name);
+        printf("No agrs need to be passed to main() in %s\n", main_file_name);
       break;
     }
     case '?':
-      if (optopt == 'f' || optopt == 't')
+      if (optopt == 'f' || optopt == 'p')
         fprintf(stderr, "Option -%c requires an argument.\n", optopt);
       else if (isprint(optopt))
         fprintf(stderr, "Unknown option `-%c'.\n", optopt);
--- 01_orcjit_class/orcjit.h
+++ 01_orcjit_class/orcjit.h
@@ -1,10 +1,14 @@
+/* Ref: https://github.com/llvm/llvm-project/blob/main/llvm/examples/Kaleidoscope/BuildingAJIT/ */
+
 #ifndef ORCJIT_ORCJIT_H
 #define ORCJIT_ORCJIT_H
 
 #include "llvm/ADT/StringRef.h"
 #include "llvm/ExecutionEngine/JITSymbol.h"
+#include "llvm/ExecutionEngine/Orc/CompileOnDemandLayer.h"
 #include "llvm/ExecutionEngine/Orc/CompileUtils.h"
 #include "llvm/ExecutionEngine/Orc/Core.h"
+#include "llvm/ExecutionEngine/Orc/EPCIndirectionUtils.h"
 #include "llvm/ExecutionEngine/Orc/ExecutionUtils.h"
 #include "llvm/ExecutionEngine/Orc/ExecutorProcessControl.h"
 #include "llvm/ExecutionEngine/Orc/IRCompileLayer.h"
@@ -22,110 +26,135 @@
 
 namespace llvm
 {
-  namespace orc
-  {
-
-    class OrcJIT
+    namespace orc
     {
-    private:
-      std::unique_ptr<ExecutionSession> ES;
 
-      DataLayout DL;
-      MangleAndInterner Mangle;
+        class OrcJIT
+        {
+        private:
+            std::unique_ptr<ExecutionSession> ES;
+            std::unique_ptr<EPCIndirectionUtils> EPCIU;
 
-      RTDyldObjectLinkingLayer ObjectLayer;
-      IRCompileLayer CompileLayer;
-      IRTransformLayer OptimizeLayer;
+            DataLayout DL;
+            MangleAndInterner Mangle;
 
-      JITDylib &MainJD;
+            RTDyldObjectLinkingLayer ObjectLayer;
+            IRCompileLayer CompileLayer;
+            IRTransformLayer OptimizeLayer;
+            CompileOnDemandLayer CODLayer;
 
-    public:
-      OrcJIT(std::unique_ptr<ExecutionSession> ES,
-             JITTargetMachineBuilder JTMB, DataLayout DL)
-          : ES(std::move(ES)), DL(std::move(DL)), Mangle(*this->ES, this->DL),
-            ObjectLayer(*this->ES,
-                        []()
-                        { return std::make_unique<SectionMemoryManager>(); }),
-            CompileLayer(*this->ES, ObjectLayer,
-                         std::make_unique<ConcurrentIRCompiler>(std::move(JTMB))),
-            OptimizeLayer(*this->ES, CompileLayer, optimizeModule),
-            MainJD(this->ES->createBareJITDylib("<main>"))
-      {
-        MainJD.addGenerator(
-            cantFail(DynamicLibrarySearchGenerator::GetForCurrentProcess(
-                DL.getGlobalPrefix())));
-      }
+            JITDylib &MainJD;
 
-      ~OrcJIT()
-      {
-        if (auto Err = ES->endSession())
-          ES->reportError(std::move(Err));
-      }
+            static void handleLazyCallThroughError()
+            {
+                errs() << "LazyCallThrough error: Could not find function body";
+                exit(1);
+            }
 
-      static Expected<std::unique_ptr<OrcJIT>> Create()
-      {
-        auto EPC = SelfExecutorProcessControl::Create();
-        if (!EPC)
-          return EPC.takeError();
+        public:
+            OrcJIT(std::unique_ptr<ExecutionSession> ES,
+                   std::unique_ptr<EPCIndirectionUtils> EPCIU,
+                   JITTargetMachineBuilder JTMB, DataLayout DL)
+                : ES(std::move(ES)), EPCIU(std::move(EPCIU)), DL(std::move(DL)),
+                  Mangle(*this->ES, this->DL),
+                  ObjectLayer(*this->ES,
+                              []()
+                              { return std::make_unique<SectionMemoryManager>(); }),
+                  CompileLayer(*this->ES, ObjectLayer,
+                               std::make_unique<ConcurrentIRCompiler>(std::move(JTMB))),
+                  OptimizeLayer(*this->ES, CompileLayer, optimizeModule),
+                  CODLayer(*this->ES, OptimizeLayer,
+                           this->EPCIU->getLazyCallThroughManager(),
+                           [this]
+                           { return this->EPCIU->createIndirectStubsManager(); }),
+                  MainJD(this->ES->createBareJITDylib("<main>"))
+            {
+                MainJD.addGenerator(
+                    cantFail(DynamicLibrarySearchGenerator::GetForCurrentProcess(
+                        DL.getGlobalPrefix())));
+            }
 
-        auto ES = std::make_unique<ExecutionSession>(std::move(*EPC));
+            ~OrcJIT()
+            {
+                if (auto Err = ES->endSession())
+                    ES->reportError(std::move(Err));
+                if (auto Err = EPCIU->cleanup())
+                    ES->reportError(std::move(Err));
+            }
 
-        JITTargetMachineBuilder JTMB(
-            ES->getExecutorProcessControl().getTargetTriple());
+            static Expected<std::unique_ptr<OrcJIT>> Create()
+            {
+                auto EPC = SelfExecutorProcessControl::Create();
+                if (!EPC)
+                    return EPC.takeError();
 
-        auto DL = JTMB.getDefaultDataLayoutForTarget();
-        if (!DL)
-          return DL.takeError();
+                auto ES = std::make_unique<ExecutionSession>(std::move(*EPC));
 
-        return std::make_unique<OrcJIT>(std::move(ES), std::move(JTMB),
-                                        std::move(*DL));
-      }
+                auto EPCIU = EPCIndirectionUtils::Create(ES->getExecutorProcessControl());
+                if (!EPCIU)
+                    return EPCIU.takeError();
 
-      const DataLayout &getDataLayout() const { return DL; }
+                (*EPCIU)->createLazyCallThroughManager(
+                    *ES, pointerToJITTargetAddress(&handleLazyCallThroughError));
 
-      /*ExecutionSession &getExecutionSession() { return *ES; }*/
+                if (auto Err = setUpInProcessLCTMReentryViaEPCIU(**EPCIU))
+                    return std::move(Err);
 
-      JITDylib &getMainJITDylib() { return MainJD; }
+                JITTargetMachineBuilder JTMB(
+                    ES->getExecutorProcessControl().getTargetTriple());
 
-      Error addModule(ThreadSafeModule TSM, ResourceTrackerSP RT = nullptr)
-      {
-        if (!RT)
-          RT = MainJD.getDefaultResourceTracker();
+                auto DL = JTMB.getDefaultDataLayoutForTarget();
+                if (!DL)
+                    return DL.takeError();
 
-        return OptimizeLayer.add(RT, std::move(TSM));
-      }
+                return std::make_unique<OrcJIT>(std::move(ES), std::move(*EPCIU),
+                                                std::move(JTMB), std::move(*DL));
+            }
 
-      Expected<JITEvaluatedSymbol> lookup(StringRef Name)
-      {
-        return ES->lookup({&MainJD}, Mangle(Name.str()));
-      }
+            const DataLayout &getDataLayout() const { return DL; }
 
-    private:
-      static Expected<ThreadSafeModule>
-      optimizeModule(ThreadSafeModule TSM, const MaterializationResponsibility &R)
-      {
-        TSM.withModuleDo([](Module &M)
-                         {
-      // Create a function pass manager.
-      auto FPM = std::make_unique<legacy::FunctionPassManager>(&M);
+            JITDylib &getMainJITDylib() { return MainJD; }
 
-      // Add some optimizations.
-      FPM->add(createInstructionCombiningPass());
-      FPM->add(createReassociatePass());
-      FPM->add(createGVNPass());
-      FPM->add(createCFGSimplificationPass());
-      FPM->doInitialization();
+            Error addModule(ThreadSafeModule TSM, ResourceTrackerSP RT = nullptr)
+            {
+                if (!RT)
+                    RT = MainJD.getDefaultResourceTracker();
 
-      // Run the optimizations over all functions in the module being added to
-      // the JIT.
-      for (auto &F : M)
-        FPM->run(F); });
+                return OptimizeLayer.add(RT, std::move(TSM));
+            }
 
-        return std::move(TSM);
-      }
-    };
+            Expected<JITEvaluatedSymbol> lookup(StringRef Name)
+            {
+                return ES->lookup({&MainJD}, Mangle(Name.str()));
+            }
 
-  } // end namespace orc
+        private:
+            static Expected<ThreadSafeModule>
+            optimizeModule(ThreadSafeModule TSM, const MaterializationResponsibility &R)
+            {
+                TSM.withModuleDo([](Module &M)
+                                 {
+                                     // Create a function pass manager.
+                                     auto FPM = std::make_unique<legacy::FunctionPassManager>(&M);
+
+                                     // Add some optimizations.
+                                     FPM->add(createInstructionCombiningPass());
+                                     FPM->add(createReassociatePass());
+                                     FPM->add(createGVNPass());
+                                     FPM->add(createCFGSimplificationPass());
+                                     FPM->doInitialization();
+
+                                     // Run the optimizations over all functions in the module being added to
+                                     // the JIT.
+                                     for (auto &F : M)
+                                         FPM->run(F);
+                                 });
+
+                return std::move(TSM);
+            }
+        };
+
+    } // end namespace orc
 } // end namespace llvm
 
 #endif // ORCJIT_ORCJIT_H
